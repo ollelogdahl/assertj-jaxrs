341e6c8490509f5361c55ac619721926373d4937
Olle Lögdahl <olle.logdahl@umu.se>
2021-06-14 13:42:49 +0200

    Added asNodeList to xpathExpression.

--- assertj-jaxrs-xml/src/test/java/com/github/attiand/assertj/jaxrs/xml/asserts/NodeAssertTest.java
+++ assertj-jaxrs-xml/src/test/java/com/github/attiand/assertj/jaxrs/xml/asserts/NodeAssertTest.java
@@ -39,6 +39,15 @@ class NodeAssertTest {
 
 	}
 
+	@Test
+	void shouldAcceptNodeList() throws ParserConfigurationException {
+		Node node = createDocument();
+
+		assertThat(node).xpath("test/multipleperson").asNodeList().allSatisfy(n -> {
+			assertThat(n).asString().isEqualTo("Bill");
+		});
+	}
+
 	private static Document createDocument() throws ParserConfigurationException {
 		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
@@ -53,6 +62,13 @@ class NodeAssertTest {
 		Element name = doc.createElement("name");
 		name.setTextContent("John");
 		person.appendChild(name);
+
+		Element otherPerson1 = doc.createElement("multipleperson");
+		otherPerson1.setTextContent("Bill");
+		root.appendChild(otherPerson1);
+		Element otherPerson2 = doc.createElement("multipleperson");
+		otherPerson2.setTextContent("Bill");
+		root.appendChild(otherPerson2);
 
 		return doc;
 	}
